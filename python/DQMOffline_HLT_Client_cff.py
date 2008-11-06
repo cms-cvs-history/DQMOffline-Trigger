5eebb7212f7f79b02ff2487f7b6b9de5a13097ce
cmsbuild <>
2008-11-06 01:24:12 +0000

    This commit was manufactured by cvs2git to create tag 'DQMOffline- Trigger-V03-03-08'.
    Sprout from master 2008-11-06 01:24:11 UTC Michail Bachtis <sha1-eaf6ea55826e5289c86058cfc492a286c076afdf@cern.ch> 'Fixed Tau harvester to not crash when there are no histos in EDM file'
    Delete:
        BuildFile
        data/EgHLTOffClient.cfi
        data/EgammaHLTOffline.cfi
        data/FourVectorHLTOffline.cfi
        data/HLT_FourVector_QualityTests.xml
        data/L1TMonitor_dqmoffline.cff
        doc/html/index.html
        doc/html/overview.html
        interface/ComCodes.h
        interface/CutCodes.h
        interface/CutValues.h
        interface/DebugFuncs.h
        interface/EgHLTDQMCut.h
        interface/EgHLTOffData.h
        interface/EgHLTOffEle.h
        interface/EgHLTOffEleSel.h
        interface/EgHLTOffHelper.h
        interface/EgHLTOffVarMon.h
        interface/EgHLTOfflineClient.h
        interface/EgammaHLTEffSrc.h
        interface/EgammaHLTOffline.h
        interface/EleHLTFilterMon.h
        interface/EleHLTPathMon.h
        interface/EleTypeCodes.h
        interface/FourVectorHLTClient.h
        interface/FourVectorHLTOffline.h
        interface/HLTEventInfoClient.h
        interface/HLTTauCaloDQMOfflineSource.h
        interface/HLTTauDQMOfflineSource.h
        interface/HLTTauElDQMOfflineSource.h
        interface/HLTTauL1DQMOfflineSource.h
        interface/HLTTauPostProcessor.h
        interface/HLTTauRefProducer.h
        interface/HLTTauTrkDQMOfflineSource.h
        interface/MonElemContainer.h
        interface/MonElemFuncs.h
        interface/MonElemManager.h
        interface/MonElemMgrEBEE.h
        interface/MonElemWithCut.h
        interface/MonElemWithCutEBEE.h
        interface/ParticlePair.h
        interface/TrigCodes.h
        python/DQMOffline_HLT_Client_cff.py
        python/DQMOffline_Trigger_Client_cff.py
        python/DQMOffline_Trigger_cff.py
        python/EgHLTOffClient_cfi.py
        python/EgammaHLTOffline_cfi.py
        python/FourVectorHLTOfflineClient_cff.py
        python/FourVectorHLTOfflineClient_cfi.py
        python/FourVectorHLTOffline_cfi.py
        python/HLTEventInfoClient_cfi.py
        python/HLT_DQM_Offline_cff.py
        python/L1TEmulatorMonitor_dqmoffline_cff.py
        python/L1TMonitor_dqmoffline_cff.py
        python/Tau/HLTTauDQMOfflineDoubleTau_cfi.py
        python/Tau/HLTTauDQMOfflineDoubleTau_cfi.pyc
        python/Tau/HLTTauDQMOfflineElectronTau_cfi.py
        python/Tau/HLTTauDQMOfflineElectronTau_cfi.pyc
        python/Tau/HLTTauDQMOfflineL1Tau_cfi.py
        python/Tau/HLTTauDQMOfflineL1Tau_cfi.pyc
        python/Tau/HLTTauDQMOfflineMuonTau_cfi.py
        python/Tau/HLTTauDQMOfflineMuonTau_cfi.pyc
        python/Tau/HLTTauDQMOfflineSingleTauMET_cfi.py
        python/Tau/HLTTauDQMOfflineSingleTauMET_cfi.pyc
        python/Tau/HLTTauDQMOfflineSingleTau_cfi.py
        python/Tau/HLTTauDQMOfflineSingleTau_cfi.pyc
        python/Tau/HLTTauDQMOffline_cff.py
        python/Tau/HLTTauDQMOffline_cff.pyc
        python/Tau/HLTTauReferences_cfi.py
        python/Tau/HLTTauReferences_cfi.pyc
        python/Tau/__init__.py
        python/Tau/__init__.pyc
        python/__init__.py
        python/__init__.pyc
        test/DQM_V0001_HLTOffline_R000000001_RelValZeeZmmMinBias.root
        test/HLTTauOfflineDQMTest_cfg.py
        test/egHLTOffDQMTest.cfg
        test/egHLTOffDQMTest_cfg.py
        test/reference_DQM_V0001_HLTOffline_R000000001_RelValZeeZmmMinBias.root
        test/reference_DQM_V0001_R000067141__GlobalCruzet4-A__CMSSW_2_1_X-Testing__RECO.root
        test/trigger_dqmoffline_sourceclient-file.cfg
        test/trigger_dqmoffline_sourceclient-file_cfg.py
        test/trigger_dqmoffline_sourceclient_CRUZET_cfg.py
        test/trigger_dqmoffline_sourceclient_relval-file_cfg.py

--- python/DQMOffline_HLT_Client_cff.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from DQMOffline.Trigger.FourVectorHLTOfflineClient_cff import *
-#from DQMOffline.Trigger.HLTEventInfoOfflineClient_cfi import *
-from DQM.HLTEvF.HLTEventInfoClient_cfi import *
-
-hltOfflineDQMClient = cms.Sequence(hltFourVectorSeqClient*hltEventInfoClient)
--- python/FourVectorHLTOfflineClient_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-hltFourVectorClient = cms.EDFilter("FourVectorHLTClient",
-    hltClientDir = cms.untracked.string('HLTOffline/FourVectorHLTOfflineClient/'),
-    hltSourceDir = cms.untracked.string('HLTOffline/FourVectorHLTOfflinehltResults'),
-    prescaleLS = cms.untracked.int32(-1),
-    prescaleEvt = cms.untracked.int32(1)
-)
-
